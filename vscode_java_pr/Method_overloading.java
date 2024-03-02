960ae027418617219f8cedb1b78ed8e9ae073e44
MadhuryaVeera <132641784+MadhuryaVeera@users.noreply.github.com>
2024-03-02 10:13:57 +0530

    Update Method_overloading.java

--- vscode_java_pr/Method_overloading.java
+++ vscode_java_pr/Method_overloading.java
@@ -2,7 +2,7 @@ public class Method_overloading {
     static void add(int a, int b){
         System.out.println(a+b);
     }
-      void add(int a,int b,int c){
+      static void add(int a,int b,int c){
         System.out.println(a+b+c);
 
     }
